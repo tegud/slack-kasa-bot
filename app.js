88a9b43d21237895fd3a3b8d9b4dca043f559212
Steve Elliott <steveelliot@evergreenenergy.co.uk>
2021-02-27 12:45:15 +0000

    trip back action handlre

--- app.js
+++ app.js
@@ -59,14 +59,6 @@ app.message('list devices', async ({ say }) => {
   }
 });
 
-app.action({ callback_id: 'toggle-device' }, async ({ body, action, ack, say }) => {
-  const [deviceId, targetStatus] = action.value.split('::');
-  console.log(deviceId, targetStatus);
-  // await say(`<@${body.user.id}> clicked the button`);
-
-  // Acknowledge the action after say() to exit the Lambda process
-  await ack();
-});
 
 module.exports.handler = serverlessExpress({
   app: expressReceiver.app
